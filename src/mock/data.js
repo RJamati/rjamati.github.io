5a2101569448827334f1d49fad82398a38419776
Rukhsar Jamati <rujamati@gmail.com>
2021-08-20 22:46:25 +0100

    Add content for new projects + linkedin

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,17 +2,17 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'Rukhsar Jamati | Software Developer based in Leicester, UK', // e.g: 'Name | Developer'
+  title: 'Rukhsar Jamati | Software Engineer based in Leicester, UK', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description:
-    'Rukhsar Jamati - A Software Developer based in Leicester, United Kingdom. Specialising in Mobile Android & iOS App Development', // e.g: Welcome to my website
+    'Rukhsar Jamati - A Software Engineer based in Leicester, United Kingdom.', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: "Hello! I'm",
   name: 'Rukhsar Jamati',
-  subtitle: 'A Software Developer who enjoys working with Android, iOS & Java.',
+  subtitle: 'A Software Engineer who enjoys Web and Mobile App Development.',
   cta: 'More about me',
 };
 
@@ -20,9 +20,9 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpeg',
   paragraphOne:
-    'A graduate with a Bachelors of Engineering in Information Technology, based in Leicester, United Kingdom.',
+    'A graduate with a Bachelors of Engineering Degree in Information Technology, based in Leicester, United Kingdom.',
   paragraphTwo:
-    'Hobbies include reading, gaming, watching TV & movies but my main passion is Mobile app development for Android and iOS.',
+    'Hobbies include reading, gaming, watching TV & movies but my main passion is coding.',
   paragraphThree:
     'I am always looking to acquire new skills and gain more experience. Check out some of my projects below.',
   resume: 'https://play.google.com/store/apps/developer?id=Rukhsar+Jamati', // if no resume, the button will not show up
@@ -33,20 +33,53 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'android-tip-calculator.png',
-    title: 'Android Tip Calculator',
+    title: 'Android - Tip Calculator',
     info:
-      'An intuitive and easy to use Tip Calculator app which allows the user to calculate the tip and also split the bill between a number of people.',
+      'An intuitive and easy to use Tip Calculator app which allows the user to calculate the tip and also split the bill between the number of people.',
     info2:
-      'This Android app is created by me using Android Studio and published on the Google Play Store. See the link below.',
+      'This Android app was created by me using Android Studio and published on the Google Play Store. See the link below.',
     url: 'https://play.google.com/store/apps/details?id=com.appdevrj.tipcalculator',
     repo: '',
   },
   {
     id: nanoid(),
-    img: 'android-expense-manager.jpg',
-    title: 'Android Expense Manager',
+    img: 'android-tip-calculator.png',
+    title: 'Wordpress - FXDFitness',
     info:
-      'The app allows the user to create a local account. Once logged in, the user enters their monthly income, they will then need to enter their expenses. The user can also create different categories for their expenses or income.',
+      'This website was for a gym and built using WordPress. The custom theme uses Advanced Custom Fields (ACF) in order to create bespoke, editable blocks for the client.',
+    info2:
+      'Features: Custom built theme, Custom reusable sections using Advanced Custom Fields, Contact form, Mobile responsive, BootStrap',
+    url: 'https://fxdfitness.com/',
+    repo: '',
+  },
+  {
+    id: nanoid(),
+    img: 'android-tip-calculator.png',
+    title: 'Wordpress - North Performance',
+    info:
+      'This website was for a Personal Trainer, built using WordPress. This was a custom built theme with most content manageable from the CMS.',
+    info2:
+      'Features: Custom reusable sections using Advanced Custom Fields, Contact form, Gallery, Mobile responsive, BootStrap',
+    url: 'https://www.northperformance.co.uk/',
+    repo: '',
+  },
+  {
+    id: nanoid(),
+    img: 'android-tip-calculator.png',
+    title: 'Android - Weather App',
+    info:
+      'The Android weather app was built using Android Studio with Java.',
+    info2:
+      'The app makes use of the built in GPS and Network Location in order to fetch the current weather information from the OpenWeather API.',
+    url: '',
+    repo: '',
+  },
+  {
+    id: nanoid(),
+    img: 'android-expense-manager.jpg',
+    title: 'Android - Expense Manager',
+    info:
+      'The app allows the user to create a local account. Once logged in, the user enters their monthly income, then they will need to enter their expenses. The user can also create different categories for their expenses or income.',
     info2:
       'Once the user has entered their monthly incomings and expenses, the app will present a pie chart to represent the data.',
     url: '',
@@ -55,7 +88,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'movie-booking.jpg',
-    title: 'ASP.NET Online Movie Ticket Booking System',
+    title: 'ASP.NET - Online Movie Ticket Booking System',
     info:
       'This Movie Ticket Booking System was my final year project for my university course. Built from scratch using ASP .NET.',
     info2:
@@ -66,7 +99,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'RBDIMS.jpg',
-    title: 'ASP.NET Registration of Birth & Death Information Management System',
+    title: 'ASP.NET - Registration of Birth & Death Information Management System',
     info:
       'This web app was the final year project for my College Diploma course. It was built using ASP .NET. The app was designed for the scenario of a local council who requires a system in order to enter residents births and deaths as well as generate certificates for them.',
     info2:
@@ -77,7 +110,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'ios-weather.jpg',
-    title: 'iOS Weather App',
+    title: 'iOS - Weather App',
     info:
       'This weather app was built as part of my iOS app development learning. Created using Xcode and Swift.',
     info2:
@@ -88,7 +121,7 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'ios-xylophone.jpg',
-    title: 'iOS Xylophone App',
+    title: 'iOS - Xylophone App',
     info:
       'This app was built as a coding exercise whilst learning iOS app development. It was built using Xcode and Swift.',
     info2:
@@ -118,11 +151,11 @@ export const footerData = {
     //   name: 'codepen',
     //   url: '',
     // },
-    // {
-    //   id: nanoid(),
-    //   name: 'linkedin',
-    //   url: '',
-    // },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/rukhsar-jamati',
+    },
     {
       id: nanoid(),
       name: 'github',
